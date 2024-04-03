fd43a5e500a93d1defd6074401e2ada2b62f644a
Pushkar Kulkarni <pushkar.kulkarni@canonical.com>
2024-04-03 11:42:04 +0530

    Add Provider class and tests

--- src/java/com/canonical/openssl/signature/SignatureRSA.java
+++ src/java/com/canonical/openssl/signature/SignatureRSA.java
@@ -1,6 +1,6 @@
 package com.canonical.openssl.signature;
 
-final class SignatureRSA extends OpenSSLSignature {
+public final class SignatureRSA extends OpenSSLSignature {
     protected String getSignatureName() {
         return "RSA";
     }
